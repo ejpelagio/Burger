38537648cfc936e4df17aafadc1dfe6e6842a0a9
ejpelagio <ejpelagio@yahoo.com>
2016-12-13 19:21:21 -0600

    db

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name) VALUES ('Bacon Jalapeno Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Buffalo Burger');
+INSERT INTO burgers(burger_name) VALUES ('Mushroom Pepper Jack Burger');
+INSERT INTO burgers(burger_name) VALUES ('Chipotle Ranch Burger');
