6e1d1b0c552d2c682eebb227004d13dcd53a11c3
ejpelagio <ejpelagio@yahoo.com>
2016-12-15 01:25:05 -0600

    burgers

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,22 @@
+//require ORM
+var orm = require('../config/orm.js');
+
 var express = require('express');
 var router = express.Router();
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var burger = require('../models/burger.js');
+
+//EXPORTING object = BURGER.
+var burger = {
+
+		selectAll: function (table, callback){
+					var queryString = 'SELECT * FROM' + table + ';';
+
+					connection.query(queryString, function (err, result){
+						if (err) throw err;
+						callback(result);
+					});
+		},
+		
+}
