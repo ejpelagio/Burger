95986ec9cadfcd8b627c12726e02f2a553231f05
ejpelagio <ejpelagio@yahoo.com>
2016-12-15 01:07:35 -0600

    orm update

--- config/orm.js
+++ config/orm.js
@@ -2,6 +2,45 @@
 var connection = require('../config/connection.js');
 
 var orm ={ 
-	selectWhere: function(table, callback)
+	selectAll: function(table, callback){
+				var queryString = 'SELECT * FROM' + table + ";";
 
-}
+				connection.query(queryString, function(err, result){
+					if (err) throw err;
+					callback(result);
+					});
+	},
+
+	insertOne: function(table, col, vals, callback){
+				var queryString = 'INSERT INTO' + table + ('+ cols =') + "VALUES('" = [vals] + "')";
+				console.log(queryString);
+
+				connection.query(queryString, vals, function(err, result){
+					if (err) throw err;
+					callback(result);
+				});
+		
+	},
+
+	updateOne: function(table, colName, burgerId, callback){
+				var queryString = "UPDATE" + table + "SET" + colName + !!!! + "WHERE id=" + burgerId;
+				console.log(queryString);
+
+				connection.query(queryString, function(err, result){
+					if (err) throw err;
+					callback(result);
+				});
+	},
+	deleteOne: function(table, burgerId, callback){
+				var queryString = "DELETE FROM" + table + "WHERE id = " + burgerId;
+				console.log(queryString);
+
+				connection.query(queryString, function(err, result){
+					if (err) throw err;
+					callback(result)
+				});
+	}
+
+};
+
+module.exports = orm; 
